c4696fb0425de9689bc4b13815a13a9f5e40ca38
Geoffrey Bii <geoffrey.bii@student.moringaschool.com>
2023-09-23 01:25:32 +0300

    submit assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 from flask_migrate import Migrate
 
 from models import db, Message
@@ -9,18 +9,83 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-CORS(app)
+# CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    message =[]
+    if request.method == 'GET':
+       
+        for me in Message.query.all():
+            me_dict = me.to_dict()
+            message.append(me_dict)
+            
+        response = make_response(
+            jsonify(message), 201, {"Content-Type": "application/json"}
+        )
+        return response
+    elif request.method == 'POST': 
+        new_message = Message(
+            body=request.get_json()["body"], username=request.get_json()["username"]
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        new_message_dict = new_message.to_dict()
+        response = make_response(new_message_dict, 201)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+   
+    
+   
+    
+   
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET', 'DELETE', 'PATCH'  ])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if not message:
+        return jsonify({"message": "Message not found"}), 404
+
+    if request.method == 'GET':
+        me_dict = message.to_dict()
+        response = make_response(
+            jsonify(me_dict),
+            200  
+        )
+        return response
+    
+    elif request.method == 'DELETE':
+                db.session.delete(message)
+                db.session.commit()
+
+                response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."    
+        }
+
+                response = make_response(
+            jsonify(response_body),
+            200
+        )
+                return response
+            
+    
+        
+        
+    elif request.method == 'PATCH':
+        for attr in request.get_json():
+            setattr(message, attr, request.get_json()[attr])
+        db.session.add(message)
+        db.session.commit()
+        message_dict = message.to_dict()
+        response = make_response(message_dict, 200)
+        return response
+    
+    
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5557)
